8f1461e42d8a676334768b807ea92feed7c43212
Jeremy Irvine <jeremyirvine.mmprod@gmaill.com>
2023-02-16 12:57:49 -0500

    initial commit

---
+++ src/copy.rs
@@ -0,0 +1,56 @@
+use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    ops::Deref,
+    path::{self, PathBuf},
+    rc::Rc,
+    thread::{JoinHandle, sleep},
+    time::Duration,
+};
+
+use fs_extra::dir::{copy_with_progress, CopyOptions, get_size};
+
+use crate::Args;
+
+#[derive(Clone)]
+pub struct CopyQueue {
+    source: PathBuf,
+    destinations: Vec<PathBuf>,
+}
+
+impl From<&Args> for CopyQueue {
+    fn from(a: &Args) -> Self {
+        Self {
+            source: a.copy_from.clone(),
+            destinations: a.drives.clone(),
+        }
+    }
+}
+
+impl CopyQueue {
+    ///
+    /// Starts the copy process using CopyQueue's source and destination variables
+    /// 
+    /// Callbacks:
+    /// * `onpercentage` - `|percentage: usize, source_dir: PathBuf| -> ()`
+    /// * `oncomplete`   - `|| -> ()`
+    /// 
+    pub fn start_copy(&self, onpercentage: Box<impl Fn(usize, PathBuf)>, oncomplete: Box<impl FnOnce()>)
+    {
+        let total_bytes = get_size(self.source.clone()).unwrap();
+        for dest in self.destinations.clone() {
+            let mut opt = CopyOptions {
+                overwrite: true,
+                content_only: true,
+                ..CopyOptions::new()
+            };
+            copy_with_progress(self.source.clone(), dest.clone(), &opt, |proc_info| {
+                let percentage = (proc_info.copied_bytes as f64 / total_bytes as f64) * 100.;
+                onpercentage(percentage as usize, dest.clone());
+                fs_extra::dir::TransitProcessResult::ContinueOrAbort
+            }).unwrap();
+        }
+
+        oncomplete();
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use clap::Parser;
+use crossterm::{
+    cursor::{MoveToColumn, MoveToNextLine, MoveUp},
+    execute, queue,
+    style::{Print, Color, SetForegroundColor, Stylize},
+    terminal::{Clear, ClearType},
+};
+use std::{
+    io::{stdout, Write, Stdout},
+    ops::Deref,
+    path::PathBuf,
+};
+
+use crate::copy::CopyQueue;
+
+pub mod copy;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg()]
+    pub copy_from: PathBuf,
+
+    #[arg()]
+    pub drives: Vec<PathBuf>,
+
+    #[arg(long, short)]
+    pub rename: Option<String>,
+
+    #[arg(long, short)]
+    pub yes: bool,
+}
+
+fn main() {
+    let mut args = Args::parse();
+
+    let mut copy_from = ::std::env::current_dir().expect("Failed to get current directory");
+    copy_from.push(args.copy_from.clone());
+
+    let dir = ::std::fs::read_dir(&copy_from)
+        .expect(format!("Could not open directory `{}`", copy_from.display()).as_str());
+
+    let dir_list = dir
+        .filter(|d| d.is_ok())
+        .map(|d| match d {
+            Ok(dir) => (dir.path(), dir.file_name().to_string_lossy().to_string()),
+            Err(_) => {
+                unreachable!();
+            }
+        })
+        .collect::<Vec<(PathBuf, String)>>();
+
+    print_pre_copy_status(&dir_list, &args);
+
+    if !args.yes {
+        print!(
+            "Does everything look correct? (You can disable this prompt with the `-y` flag) (Y/n) "
+        );
+        ::std::io::stdout().flush().expect("Failed to flush stdout");
+        let mut buffer = String::new();
+        ::std::io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read user input");
+
+        match buffer.replace("\r\n", "").to_lowercase().as_str() {
+            "y" | "yes" => {
+                queue!(stdout(), 
+                    MoveUp(1), 
+                    Clear(ClearType::CurrentLine), 
+                    MoveToColumn(0)
+                ).unwrap();
+
+                stdout().flush().unwrap();
+            }
+            _ => {
+                println!("[decopy] Aborting copy...");
+                ::std::process::exit(0);
+            }
+        }
+    }
+
+    let mut queue = CopyQueue::from(&args);
+    handle_copying(&mut queue);
+
+    if let Some(drive_name) = args.rename {
+        println!("[decopy] Renaming `D:\\` to `{}`", drive_name);
+    }
+}
+
+fn print_pre_copy_status(dir_list: &Vec<(PathBuf, String)>, args: &Args) {
+    log(format!("Destinations staged to be copied to:\n"));
+    for drive in args.drives.clone() {
+        println!("  {}", drive.display().to_string().dark_grey());
+    }
+    log(format!("Copying from `{}`...\n", args.copy_from.display()));
+    let (list, is_overflowing) = if dir_list.len() >= 5 {
+        (&dir_list[..5], true)
+    } else {
+        (&dir_list[..], false)
+    };
+
+    for (path, display) in list {
+        println!("  {}", display.clone().dark_grey());
+    }
+    if is_overflowing {
+        println!("  ... +{} more ...", dir_list.len() - list.len());
+    }
+}
+
+pub fn handle_copying(queue: &mut CopyQueue) {
+    // execute!(stdout(), MoveToNextLine(1)).unwrap();
+
+    let onpercentage = move |percent: usize, current_dir: PathBuf| {
+        queue!(stdout(),
+            Clear(ClearType::CurrentLine),
+            MoveToColumn(0),
+        ).unwrap();
+        log_queue(format!("Copying... ({} %) --> {}", percent, current_dir.display()));
+
+        stdout().flush().unwrap();
+    };
+
+    let oncomplete = move || {
+        queue!(stdout(), Print("\n")).unwrap();
+        log("Files finished copying");
+    };
+
+    queue.start_copy(Box::new(onpercentage), Box::new(oncomplete));
+}
+
+pub fn log_queue(msg: impl Into<String>) {
+    queue!(stdout(),
+        Print("["),
+        SetForegroundColor(Color::Magenta),
+        Print("decopy"),
+        SetForegroundColor(Color::Reset),
+        Print("] "),
+        Print(msg.into()),
+    ).unwrap();
+}
+
+pub fn log(msg: impl Into<String>) {
+    log_queue(msg);
+    stdout().flush().unwrap();
+}
